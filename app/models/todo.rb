df8de5bde6f235c6128b8c5c4831a20777ec0b20
Sean M. Snyder <sean.snyder@snhu.edu>
2016-04-22 16:55:11 -0400

    Refactor todo model/controller to use #complete!

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,10 @@
+# Todo class
 class Todo < ActiveRecord::Base
   def completed?
     completed_at?
+  end
+
+  def complete!
+    touch :completed_at
   end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
-describe Todo do
+describe Todo, '#completed?' do
   it 'returns completed if timestamp is set' do
     todo = Todo.new(completed_at: Time.current)
     expect(todo).to be_completed
   end
 
   it 'returns false when not set' do
-    todo = Todo.new()
+    todo = Todo.new
     expect(todo).to_not be_completed
+  end
+end
+
+describe Todo, '#complete!' do
+  it 'marks the todo complete' do
+    todo = Todo.create!(completed_at: nil)
+
+    todo.complete!
+
+    expect(todo).to be_completed
   end
 end
